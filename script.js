b6dacebdf264b2836668eefe9637817b2457af3a
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-09-15 05:10:18 +0600

    time spent convert integer

--- script.js
+++ script.js
@@ -67,7 +67,9 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTakenInt = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt(timeTakenInt)
+
 
   // show result modal
   resultModal.innerHTML = "";
@@ -121,6 +123,7 @@ const start = () => {
     }
     count--;
   }, 1000);
+  countdownOverlay.innerHTML = ""
 };
 
 // START Countdown
@@ -132,7 +135,8 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpentInt = (currentTime - startTime) / 1000;
+  const timeSpent = parseInt(timeSpentInt)
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
